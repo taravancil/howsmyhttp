eff1df323f6c5ae3c04e548f117c4238d17f48b7
Tara Vancil <contact@taravancil.com>
2019-11-23 11:41:27 -0500

    don't output CSS after subsetting fonts, add instructions

--- sh/subset-fonts.sh
+++ sh/subset-fonts.sh
@@ -1,1 +1,5 @@
-glyphhanger http://localhost:4000 --spider --css --family='Nunito' --subset='src/fonts/*.ttf' --formats=woff,woff2 && mv src/fonts/*-subset*.woff* static/fonts; 
+glyphhanger http://localhost:4000 --spider --family='Nunito' --subset='src/fonts/*.ttf' --formats=woff,woff2
+
+mv src/fonts/*-subset*.woff* static/fonts
+
+echo "\n→ Copy output unicode range to update the existing unicode range in src/fonts.css"
